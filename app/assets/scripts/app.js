1335a63302cdba0c5e6f02daf6166596d8529500
dariusz gorzeba <dgorzeba@gmail.com>
2022-04-14 22:34:44 +0200

    integrated openweathermap

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,13 +1,15 @@
-console.log('app.js');
-
 const api_key = '386a2f67d2da471e24a2608d4f4554c8';
-const request = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+const getTemp = async (event) => {
+  const button = event.target;
+  const coords = JSON.parse(button.value)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=${api_key}&units=metric`
+  console.log(url)
 
-const getTemp = (event) => {
-  console.log(event.target.value);
-  console.log('getting temp');
+  const response = await fetch(url);
+  const json = await response.json();
+
+  button.innerHTML = json.main.temp;
 }
 
 const buttons = document.querySelectorAll("[name='weather']");
-
-buttons.forEach(b => b.addEventListener("click", getTemp, false) );
+buttons.forEach(b => b.addEventListener("click", getTemp, false));
