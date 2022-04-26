4f79c191345f1400c023423c08b506a71e02af67
dawidmiechowski <miechowski_dawid@o2.plr>
2022-04-26 11:33:32 +0200

    if else checktemp

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,4 @@
-const api_key = '75687d9016782b963ffcb6dc0ffd382e';
+const api_key = '';
 const getTemp = async (event) => {
   const button = event.target;
   const coords = JSON.parse(button.value)
@@ -12,4 +12,31 @@ const getTemp = async (event) => {
 }
 
 const buttons = document.querySelectorAll("[name='weather']");
-buttons.forEach(b => b.addEventListener("click", getTemp, false));
+buttons.forEach(b => b.addEventListener("click", getTemp, checkTemp, false));
+
+function checkTemp(){
+  if (temp >= 0 && temp <= -5){
+    tempClass = not-cold-at-all;
+  } else if (temp >= -5 && temp <= -10){
+    tempClass = slightly-cold;
+  } else if (temp >= -10 && temp <= -15){
+    tempClass = modertaly-cold;
+  } else if (temp >= -15 && temp <= -25){
+    tempClass = very-cold;
+  } else if (temp >= -25 && temp <= -45){
+    tempClass = extremely-cold;
+  } else if (temp >= 0 && temp <= 5){
+    tempClass = not-warm-at-all;
+  } else if (temp >= 10 && temp <= 15){
+    tempClass = slightly-warm;
+  } else if (temp >= 15 && temp <= 25){
+    tempClass = very-warm;
+  } else if (temp >= 25 && temp <= 45){
+    tempClass = extremely-warm;
+  }
+}
+
+function allFunctions(){
+  getTemp();
+  checkTemp();
+}
