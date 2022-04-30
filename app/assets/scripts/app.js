6aa50d38802e5df5f208c47e50c8e6a0f4418d90
dawidmiechowski <miechowski_dawid@o2.plr>
2022-04-30 14:32:13 +0200

    btn and colors finished

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,4 @@
-const api_key = '75687d9016782b963ffcb6dc0ffd382e';
+const api_key = '';
 const getTemp = async (event) => {
   const button = event.target;
   const coords = JSON.parse(button.value)
@@ -10,6 +10,9 @@ const getTemp = async (event) => {
   const temp = json.main.temp;
 
   const stringTemp = convertTempToStringGrade(temp)
+  console.log("." + stringTemp);
+
+  button.classList.add("" + stringTemp)
 
   button.innerHTML = temp;
 }
@@ -25,12 +28,12 @@ function convertTempToStringGrade(temp){
   const temperatures = [
     ["extremely-cold", -200, -26],
     ["very-cold", -25, -16],
-    ["modertaly-cold", -15, -10],
+    ["moderately-cold", -15, -10],
     ["slightly-cold", -9, -6],
     ["not-cold-at-all",-5, 0],
     ["not-warm-at-all", 0,5],
     ["slightly-warm", 6, 10],
-    ["modertaly-warm", 11, 15],
+    ["moderately-warm", 11, 15],
     ["very-warm", 16,25],
     ["extremely-warm", 26, 49],
     ["extremely-hot", 50, 200]
@@ -40,6 +43,7 @@ function convertTempToStringGrade(temp){
     const t = temperatures[i]
     if (between(temp, t[1], t[2]))
       return t[0]
+
   }
 
 }
@@ -52,9 +56,10 @@ const tempTests = [
   [50, 'extremely-hot'],
   [3, 'not-warm-at-all'],
   [7, 'slightly-warm'],
-  [12, 'modertaly-warm'],
+  [12, 'moderately-warm'],
   [20, 'very-warm']
 ]
+
 
 tempTests.forEach(test => {
     const result = convertTempToStringGrade(test[0])
@@ -64,11 +69,11 @@ tempTests.forEach(test => {
 
 console.assert(convertTempToStringGrade(-3)  === 'not-cold-at-all', 'not-cold-at-all')
 console.assert(convertTempToStringGrade(-7) === 'slightly-cold', 'slightly-cold')
-console.assert(convertTempToStringGrade(-10) === 'modertaly-cold', 'for -10 expected modertaly-cold')
-console.assert(convertTempToStringGrade(-12) === 'modertaly-cold', 'modertaly-cold')
+console.assert(convertTempToStringGrade(-10) === 'moderately-cold', 'for -10 expected moderately-cold')
+console.assert(convertTempToStringGrade(-12) === 'moderately-cold', 'moderately-cold')
 console.assert(convertTempToStringGrade(-20) === 'very-cold', 'very-cold')
 console.assert(convertTempToStringGrade(-50) === 'extremely-cold', 'extremely-cold')
 console.assert(convertTempToStringGrade(3)  === 'not-warm-at-all', 'not-warm-at-all')
 console.assert(convertTempToStringGrade(7) === 'slightly-warm', 'slightly-warm')
-console.assert(convertTempToStringGrade(12) === 'modertaly-warm', 'modertaly-warm')
+console.assert(convertTempToStringGrade(12) === 'moderately-warm', 'moderately-warm')
 console.assert(convertTempToStringGrade(20) === 'very-warm', 'very-warm')
